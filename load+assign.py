c406f4ece6636da793e561d4d8e6d1ba8dedb18e
Schildt <schildt@e3d.rwth.aachen.de>
2022-08-15 15:55:02 +0200

    Added script_paper and load_and_assign example files

---
+++ load+assign.py
@@ -0,0 +1,37 @@
+import csv
+
+from datetime import datetime
+
+from teaser.data.dataclass import DataClass
+from teaser.logic.buildingobjects.buildingphysics.innerwall import InnerWall
+from teaser.project import Project
+from teaser.logic.buildingobjects.buildingphysics.material import Material
+from teaser.data.input.citygml_input import load_gml_lxml
+from teaser.logic.buildingobjects.buildingphysics.en15804lcadata import En15804LcaData
+import simulate as sim
+
+from pathlib import Path
+
+if __name__ == '__main__':
+    prj = Project(load_data=True)
+    prj.name = "testArchetype"
+
+    """prj.use_b4 = True  # Parameters for the calculation of the life cycle assessment. Phase "b4" according to EN15804 is used
+
+    prj.add_residential(
+        method='iwu',
+        usage='single_family_dwelling',
+        name="Typ I",
+        year_of_construction=2015,
+        number_of_floors=2,
+        height_of_floors=2.5,
+        net_leased_area=167)  # building is added. method and usage specify the enrichment method
+"""
+    load_gml_lxml(path = "C:\\Users\\feldman\\Examples_GML\\FZK-Haus-LoD2-KIT-IAI-KHH-B36-V1.gml", prj = prj, method = "tabula_de")
+    prj.calc_all_buildings()  # simulation parameters are calculated
+
+    prj.export_aixlib()  # model export
+
+    # Simulation is started. Please exchange the file paths ;)
+    sim.simulate(path="C:\\Users\\feldman\\TEASEROutput", prj=prj, loading_time=3600,
+                 result_path="C:\\Users\\feldman\\TEASEROutput\\results")
