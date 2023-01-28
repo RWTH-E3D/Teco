eb865036b215707fcedffb8695eb4870cd9f7379
yacinetayeb <yacine.tayeb@rwth-aachen.de>
2023-01-28 12:49:38 +0100

    extract all tabula_de utilities' gwp values from OEKOBAUDAT

--- teco/logic/buildingobjects/utility_test.py
+++ teco/logic/buildingobjects/utility_test.py
@@ -1,15 +1,14 @@
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
+import json
 import time
 
-# link = "https://www.oekobaudat.de/OEKOBAU.DAT/datasetdetail/process.xhtml?uuid=dcd5e23a-9bec-40b6-b07c-1642fe696a2e&version=20.19.120"
-# link = "https://webtool.building-typology.eu/data/matrix/building/de/p/0/o/0/l/10/dc/1234567890123"
-# link = "https://oekobaudat.de/OEKOBAU.DAT/resource/datastocks/cd2bda71-760b-4fcc-8a0b-3877c10000a8/processes/b7cacb37-7945-4518-be5a-bf7df7edf5c2?format=html&lang=en&version=20.19.120"
 from selenium.webdriver import ActionChains
-from sympy import false, true
+from selenium.common.exceptions import NoSuchElementException
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# stages = ["A1-A3", "A1", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "C1", "C2", "C3", "C4", "D"]
 """
 df = pd.read_csv("C:\\Users\\tayeb\\Documents\\OEKOBAUDAT.csv", delimiter=';', encoding='latin-1')
 print(df['URL'])
@@ -28,72 +27,213 @@ for link in df.loc['URL']:
 """
 
 
-def TABULA_DE(self):
+def get_indicator_values(link):
+    table = pd.read_html(link)
+
+    df = table[len(table) - 1]  # make this better?
+
+    row = df.loc[df['Indicator'] == 'Global warming potential (GWP)']
+
+    print(row.iloc[0, 0])
+
+    json_dict = {}
+
+    for stage in df.columns[2:]:
+        json_dict.update({(str.lower(stage[stage.rfind(' ') + 1:].replace("-", "_")),
+                           float((str(row.iloc[0].loc[stage])[:str(row.iloc[0].loc[stage]).find("$")]).lower()))})
+
+    return row.iloc[0].loc['Unit'], json_dict
+
+
+def TABULA_DE():
     df = pd.read_excel("C:/Users/tayeb/Documents/Teaser+,Teco/TABULA-Analyses_DE-Typology_ResultData.xlsx",
                        sheet_name=0)
-    # print(df.loc[554:1357,:])
 
+    Code_Building = df['Code_Building'][556:1359]
     Code_BuiSysCombi = df['Code_BuiSysCombi'][556:1359]
-    Description_National_SysH = df['Description_National_SysH'][556:1359]
-    Description_National_SysW = df['Description_National_SysW'][556:1359]
-    Description_National_SystemType = df['Description_National_SystemType'][556:1359]
-    Description_National_System_AssignedMeasure = df['Description_National_System_AssignedMeasure'][556:1359]
+    Description_SystemType = df['Description_SystemType'][556:1359]
+    Year1_Building = df['Year1_Building'][556:1359]
+    Year2_Building = df['Year2_Building'][556:1359]
 
-    pd.DataFrame(
-        [Code_BuiSysCombi, Description_National_SysH, Description_National_SysW, Description_National_SystemType,
-         Description_National_System_AssignedMeasure]).to_excel("sheet.xlsx")
+    with open("utilities.json", 'w') as writer:
+        writer.write(json.dumps([{Code_Building: {"building_age_group": [Year1_Building, Year2_Building],
+                                                  "Code_BuiSysCombi": Code_BuiSysCombi,
+                                                  "Description_SystemType": Description_SystemType
+                                                  }}
+                                 for
+                                 Code_Building, Code_BuiSysCombi, Description_SystemType, Year1_Building, Year2_Building
+                                 in
+                                 zip(Code_Building, Code_BuiSysCombi, Description_SystemType, Year1_Building,
+                                     Year2_Building)],
+                                ensure_ascii=False))
 
 
 """ load libraries ???"""
 
-link = "http://www.oekobaudat.de/no_cache/en/database/search.html"
-# soup = BeautifulSoup(requests.get(link).text, "html.parser")
-# OEKOBAUDAT = pd.read_html(link)
-# print(OEKOBAUDAT.columns)
+
+def get_utility_OEKOBAUDAT(utility, bereich=1):
+    link = "https://www.oekobaudat.de/no_cache/en/database/search.html"
+    driver = webdriver.Firefox()  # seconds
+    driver.get(link)
+    actions = ActionChains(driver)
+    # time.sleep(3)
+    wait = WebDriverWait(driver, 10)
+
+    # click on category browser
+    button = wait.until(EC.element_to_be_clickable(
+        (By.XPATH, '//*[@id="c1552"]/s4lca-root-0/div/s4lca-process/div/p-table/div/div[1]/div[2]/p-togglebutton')))
+    button.click()
+
+    # click on 8. Building service engineering
+    element = wait.until(EC.presence_of_element_located(
+        (By.XPATH, "//div[contains(@class, 'ui-treenode-content')]//*[text()[contains(., 'engineering')]]/../../..")))
+    actions.move_to_element(element).click().perform()
+
+    # open tree
+    element = WebDriverWait(element, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'ui-tree-toggler')))
+    actions.move_to_element(element).click().perform()
+
+    # click on Heating
+    element = WebDriverWait(element, 10).until(EC.presence_of_element_located(
+        (By.XPATH, "//ul[contains(@class, 'ui-treenode-children')]//*[text()[contains(., 'Heating')]]")))
+    # element = element.find_element(By.XPATH, '//*[@id="lang-en"]/div[3]/div/div[2]/div/div[2]/s4lca-categories/p-tree/div/div/ul/p-treenode[8]/li/ul/p-treenode[1]/li/div/span[1]')
+    actions.move_to_element(element).click().perform()
+
+    # use search bar to find utility object in OEKOBAUDAT
+    element = element.find_element(By.XPATH,
+                                   f"//div[contains(@id,'bereich{bereich}')]//input[contains(@data-testid,'column-name-input')]")
+    element.send_keys(str(utility))
+
+    # make sure the required utility is loaded
+    try:
+        elementwait = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,
+                                                                                      f"//div[contains(@id,'bereich{bereich}')]//tr/*//a[contains(text(),'{utility}')]")))
+    finally:
+        # get attribute link
+        element = element.find_element(By.XPATH,
+                                       f"//div[contains(@id,'bereich{bereich}')]//tr/*//a[contains(text(),'{utility}')]")
+        try:
+            utility_link = element.get_attribute('href')
+        except NoSuchElementException:
+            print("utility not found")
+    # click to open page
+    # element = element.find_element(By.XPATH, f"//div[contains(@id,'bereich{bereich}')]//tr/*//a[contains(text(),'Liquefied gas tank 2700')]")
+    # element.click()
+
+    # open Heating tree?
+    time.sleep(1)
+    driver.close()
+    return utility_link
 
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import ElementNotInteractableException
+from teco.logic.buildingobjects.buildingsystems.utility import Utility
+from teaser.logic.buildingobjects.building import Building
+import teco.data.input.utility_input_json
 
-driver = webdriver.Firefox()  # seconds
-driver.get(link)
+# TABULA_DE()  # loads to utilities.json
+df = pd.read_excel("C:/Users/tayeb/Documents/Teaser+,Teco/TABULA-Analyses_DE-Typology_ResultData.xlsx",
+                   sheet_name=0)
 
-elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "ui-table-tbody")))
-
-print(driver.title)
-# print(driver.page_source)
-button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="c1552"]/s4lca-root-0/div/s4lca-process/div/p-table/div/div[1]/div[2]/p-togglebutton')))
-print(button.click())
+Code_Building = df['Code_Building'][556:1359]
+Code_BuiSysCombi = df['Code_BuiSysCombi'][556:1359]
+Description_SysH = df['Description_SysH'][556:1359]
+Description_SysW = df['Description_SysW'][556:1359]
+Year1_Building = df['Year1_Building'][556:1359]
+Year2_Building = df['Year2_Building'][556:1359]
 
 
-actions = ActionChains(driver)
+dict_SysH = {'EPDM foam': 'poor insulation of pipes',  # /good insulation of pipes
+             # 'Liquefied gas tank': '???',
+             'Underfloor heating': 'heat source ground',  # not sure
+             'Gas heat pump': 'air source heat pump',  # not sure
+             # 'Woodchip boiler': '???',
+             'Pellet boiler': 'woodpellets boiler',  # whats the difference?
+             # 'Oil tank': '???',
+             'Electric heat pump': 'electrical heatpump'
+             # 'Chimney', 'Transfer station', 'Circulating pump'}
+             }
 
-element = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[2]/div/div[2]/s4lca-categories/p-tree/div/div/ul/p-treenode[8]/li/div/span')
-actions.move_to_element(element).click().perform()
+dict_SysW = {'Elelectric continuous flow heaters': 'decentral electric: instantaneous water heaters',
+             'solar collector': 'solar'
+             }
 
-#if element.find_element(By.TAG_NAME, 'aria-expanded'):
-element = element.find_element(By.XPATH, '//*[@id="lang-en"]/div[3]/div/div[2]/div/div[2]/s4lca-categories/p-tree/div/div/ul/p-treenode[8]/li/ul/p-treenode[1]/li/div')
-actions.move_to_element(element).click().perform()
+json_dict = {}
+
+for Code_Building, Code_BuiSysCombi, Description_SysH, Description_SysW, Year1_Building, Year2_Building in zip(
+        Code_Building, Code_BuiSysCombi, Description_SysH, Description_SysW, Year1_Building, Year2_Building):
+
+    json_dict.update({Code_Building: {"Building_age_group": [Year1_Building, Year2_Building],
+                                      "Code_BuiSysCombi": Code_BuiSysCombi,
+                                      "Description_SystemHeating": Description_SysH,
+                                      "Description_SystemWaterHeating": Description_SysW,
+                                      "Utilities": '',
+                                      "lca_data": {},
+                                      "Unit": ''
+                                      }})
+
+    print(Description_SysH)
+
+    # check for gas/oil condensing boiler case
+    if 'gas central heating' in Description_SysH:
+        if 'condensing boiler' in Description_SysH:
+            json_dict[Code_Building]["Utilities"] = 'Gas condensing boiler'
+            link = get_utility_OEKOBAUDAT('Gas condensing boiler')
+            json_dict[Code_Building]["Unit"], json_dict[Code_Building]["lca_data"] = get_indicator_values(link)
+
+        elif 'low temperature' in Description_SysH:
+            json_dict[Code_Building]["Utilities"] = 'Gas low temperature'
+            link = get_utility_OEKOBAUDAT('Gas low temperature')
+            json_dict[Code_Building]["Unit"], json_dict[Code_Building]["lca_data"] = get_indicator_values(link)
+
+    if 'oil central heating' in Description_SysH:
+        if 'condensing boiler' in Description_SysH:
+            json_dict[Code_Building]["Utilities"] = 'Oil condensing boiler'
+            link = get_utility_OEKOBAUDAT('Oil condensing boiler')
+            json_dict[Code_Building]["Unit"], json_dict[Code_Building]["lca_data"] = get_indicator_values(link)
+        elif 'low temperature' in Description_SysH:
+            json_dict[Code_Building]["Utilities"] = 'Oil low temperature'
+            link = get_utility_OEKOBAUDAT('Oil low temperature')
+            json_dict[Code_Building]["Unit"], json_dict[Code_Building]["lca_data"] = get_indicator_values(link)
+
+    for utility in dict_SysH:
+        if dict_SysH[utility] in Description_SysH:
+            json_dict[Code_Building]["Utilities"] = utility
+            link = get_utility_OEKOBAUDAT(utility)
+            json_dict[Code_Building]["Unit"], json_dict[Code_Building]["lca_data"] = get_indicator_values(link)
+
+    print(Description_SysW)
+
+    for utility in dict_SysW:
+        if dict_SysW[utility] in Description_SysW:
+            json_dict[Code_Building]["Utilities"] = utility
+            link = get_utility_OEKOBAUDAT(utility)
+            json_dict[Code_Building]["Unit"], json_dict[Code_Building]["lca_data"] = get_indicator_values(link)
+
+with open("utilities.json", 'w') as writer:
+    writer.write(json.dumps(json_dict, ensure_ascii=False))
+# utility.add_utilities_to_json()
+# divide archetypes from excel sheet according to building age class, then, for each building class, for each archetype, get utility objects
+# build utility_input_json for this? based on buildingelement_input_json.py
 
 
-#next_button = driver.find_element(By.CSS_SELECTOR, 'p-treenode.ng-star-inserted:nth-child(9) > li:nth-child(1) > div:nth-child(1) > span:nth-child(3) > span:nth-child(1) > span:nth-child(1)')
+# use code in utility.add_utilities_to_archetypes() to get gwp values
 
-#element = driver.find_element(By.CSS_SELECTOR, "p-treenode.ng-star-inserted:nth-child(8)>li:nth-child(1)>div:nth-child(1)>span:nth-child(3)>span:nth-child(1)>span:nth-child(1)")
-#element_ = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[2]/div/div[2]/s4lca-categories/p-tree/div/div/ul/p-treenode[8]/li/div/span[3]/span/span")
-#print(element_.click())
-#print(element.click())
+# store values in en15804lcadata class object
 
-#element = driver.find_element(By.LINK_TEXT, "8. Building service engineering")
+
+# next_button = driver.find_element(By.CSS_SELECTOR, 'p-treenode.ng-star-inserted:nth-child(9) > li:nth-child(1) > div:nth-child(1) > span:nth-child(3) > span:nth-child(1) > span:nth-child(1)')
+
+# element = driver.find_element(By.CSS_SELECTOR, "p-treenode.ng-star-inserted:nth-child(8)>li:nth-child(1)>div:nth-child(1)>span:nth-child(3)>span:nth-child(1)>span:nth-child(1)")
+# element_ = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[2]/div/div[2]/s4lca-categories/p-tree/div/div/ul/p-treenode[8]/li/div/span[3]/span/span")
+# print(element_.click())
+# print(element.click())
+
+# element = driver.find_element(By.LINK_TEXT, "8. Building service engineering")
 # tree = driver.find_element(By.XPATH,
 #                           '//*[@id="lang-en"]/div[3]/div/div[2]/div/div[2]/s4lca-categories/p-tree/div/div/ul')
 # except:
 #        print("error: ")
-time.sleep(10)
-driver.close()
+
 '''
     time.sleep(1)
     Error = true
