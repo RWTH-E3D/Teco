3ed376031b4f5bf7e3d67b31ca550dd627e861d4
yacinetayeb <yacine.tayeb@rwth-aachen.de>
2023-04-04 17:34:44 +0100

    finished up example script

--- Examples/issue16_example.py
+++ Examples/issue16_example.py
@@ -1,13 +1,13 @@
-from teaser.data.input.citygml_input import load_gml_lxml
 from teco.logic.buildingobjects.buildingsystems.utility import Utility
 from teco.data.input.utility_input_json import load_en15804_lca_data
-from teaser.project import Project
+from teco.project import Project
+import simulate as sim
 
 if __name__ == '__main__':
 
     prj = Project(load_data=True)
 
-    prj.name = "test"
+    prj.name = "test_utilities_data"
 
     prj.add_residential(
         method='tabula_de',
@@ -16,10 +16,24 @@ if __name__ == '__main__':
         year_of_construction=2015,
         number_of_floors=2,
         height_of_floors=2.5,
-        net_leased_area=167)
+        net_leased_area=167.0)
 
+    prj.calc_all_buildings()  # simulation parameters are calculated
 
-    ut= Utility(parent=prj.buildings[0], name="Gas condensing boiler < 20 kW")
+    prj.export_aixlib()  # model export
 
-    load_en15804_lca_data(building_archetype="DE.N.SFH.11.Gen.ReEx.001.003",utility=ut,data_class=ut.parent.parent.data)
+    # Simulation is started. Please exchange the file paths ;)
+    sim.simulate(path="C:\\Users\\tayeb\\TEASEROutput", prj=prj, loading_time=3600,
+                 result_path="C:\\Users\\tayeb\\TEASEROutput\\test")
 
+    # Instance of Utility class is created
+    ut = Utility(parent=prj.buildings[0], name="Gas condensing boiler < 20 kW")
+
+    # load lca data from Utilities.json
+    load_en15804_lca_data(utility=ut, data_class=prj.data, building_archetype='')
+
+    # print loaded lca data to console
+    indicators = ['pere', 'perm', 'pert', 'penre', 'penrm', 'penrt', 'sm', 'rsf', 'nrsf', 'fw', 'hwd', 'nhwd', 'rwd',
+                  'cru', 'mfr', 'mer', 'eee', 'eet', 'gwp', 'odp', 'pocp', 'ap', 'ep', 'adpe', 'adpf']
+    for indicator in indicators:
+        print(indicator, ':', getattr(ut.lca_data, indicator).get_values_as_dict())
--- teco/data/input/utility_input_json.py
+++ teco/data/input/utility_input_json.py
@@ -2,20 +2,21 @@
 
 import json
 from teco.logic.buildingobjects.buildingsystems.utility import Utility
-
+from teaser.logic.archetypebuildings.tabula.de.terracedhouse import TerracedHouse
+from teaser.logic.archetypebuildings.tabula.de.apartmentblock import ApartmentBlock
+from teaser.logic.archetypebuildings.tabula.de.multifamilyhouse import MultiFamilyHouse
+from teaser.logic.archetypebuildings.tabula.de.singlefamilyhouse import SingleFamilyHouse
 from teco.logic.buildingobjects.buildingphysics.en15804indicatorvalue import En15804IndicatorValue
 
 
 
-def load_en15804_lca_data(building_archetype, utility, data_class):
+def load_en15804_lca_data(utility, data_class, building_archetype=''):
     """LCA-data loader with building archetype as identification.
 
     Loads LCA-data specified in the JSON.
 
     Parameters
     ----------
-    building_archetype : str
-        code for archetype in JSON
 
     utility : Utility()
         utility class instance
@@ -25,93 +26,57 @@ def load_en15804_lca_data(building_archetype, utility, data_class):
         (typically this is the data class stored in prj.data,
         but the user can individually change that.
 
+    building_archetype : str
+        code for archetype in JSON
     """
+
+    # build archetype code
+    if building_archetype == '':
+        building_archetype = 'DE.N.'
+        if type(utility.parent) is SingleFamilyHouse: building_archetype += 'SFH.'
+        elif type(utility.parent) is MultiFamilyHouse: building_archetype += 'MFH.'
+        elif type(utility.parent) is TerracedHouse: building_archetype += 'TH.'
+        elif type(utility.parent) is ApartmentBlock: building_archetype += 'AB.'
 
     bindings = data_class.utilities_data_bind
 
     for archetype, data in bindings.items():
-        if archetype == building_archetype:
 
-            try:
-                data = data['Utilities'][utility.name]
-            except KeyError:
-                print("Could not find utility with name: " + utility.name)
+        if building_archetype in archetype:
 
-            pere = En15804IndicatorValue()
-            perm = En15804IndicatorValue()
-            pert = En15804IndicatorValue()
-            penre = En15804IndicatorValue()
-            penrm = En15804IndicatorValue()
-            penrt = En15804IndicatorValue()
-            sm = En15804IndicatorValue()
-            rsf = En15804IndicatorValue()
-            nrsf = En15804IndicatorValue()
-            fw = En15804IndicatorValue()
-            hwd = En15804IndicatorValue()
-            nhwd = En15804IndicatorValue()
-            rwd = En15804IndicatorValue()
-            cru = En15804IndicatorValue()
-            mfr = En15804IndicatorValue()
-            mer = En15804IndicatorValue()
-            eee = En15804IndicatorValue()
-            eet = En15804IndicatorValue()
-            gwp = En15804IndicatorValue()
-            odp = En15804IndicatorValue()
-            pocp = En15804IndicatorValue()
-            ap = En15804IndicatorValue()
-            ep = En15804IndicatorValue()
-            adpe = En15804IndicatorValue()
-            adpf = En15804IndicatorValue()
+            if data['Building_age_group'][0] <= utility.parent.year_of_construction <= data['Building_age_group'][1]:
 
-            pere.set_values(**data["pere"])
-            perm.set_values(**data["perm"])
-            pert.set_values(**data["pert"])
-            penre.set_values(**data["penre"])
-            penrm.set_values(**data["penrm"])
-            penrt.set_values(**data["penrt"])
-            sm.set_values(**data["sm"])
-            rsf.set_values(**data["rsf"])
-            nrsf.set_values(**data["nrsf"])
-            fw.set_values(**data["fw"])
-            hwd.set_values(**data["hwd"])
-            nhwd.set_values(**data["nhwd"])
-            rwd.set_values(**data["rwd"])
-            cru.set_values(**data["cru"])
-            mfr.set_values(**data["mfr"])
-            mer.set_values(**data["mer"])
-            eee.set_values(**data["eee"])
-            eet.set_values(**data["eet"])
-            gwp.set_values(**data["gwp"])
-            odp.set_values(**data["odp"])
-            pocp.set_values(**data["pocp"])
-            ap.set_values(**data["ap"])
-            ep.set_values(**data["ep"])
-            adpe.set_values(**data["adpe"])
-            adpf.set_values(**data["adpf"])
+                try:
+                    data = data['Utilities'][utility.name]
+                except KeyError:
+                    print("Could not find utility: " + utility.name + "\nin archetype: " + archetype)
+                    continue
 
-            utility.lca_data.pere = pere
-            utility.lca_data.perm = perm
-            utility.lca_data.pert = pert
-            utility.lca_data.penre = penre
-            utility.lca_data.penrm = penrm
-            utility.lca_data.penrt = penrt
-            utility.lca_data.sm = sm
-            utility.lca_data.rsf = rsf
-            utility.lca_data.nrsf = nrsf
-            utility.lca_data.fw = fw
-            utility.lca_data.hwd = hwd
-            utility.lca_data.nhwd = nhwd
-            utility.lca_data.rwd = rwd
-            utility.lca_data.cru = cru
-            utility.lca_data.mfr = mfr
-            utility.lca_data.mer = mer
-            utility.lca_data.eee = eee
-            utility.lca_data.eet = eet
-            utility.lca_data.gwp = gwp
-            utility.lca_data.odp = odp
-            utility.lca_data.pocp = pocp
-            utility.lca_data.ap = ap
-            utility.lca_data.ep = ep
-            utility.lca_data.adpe = adpe
-            utility.lca_data.adpf = adpf
+                utility.lca_data.pere.set_values(**data["pere"])
+                utility.lca_data.perm.set_values(**data["perm"])
+                utility.lca_data.pert.set_values(**data["pert"])
+                utility.lca_data.penre.set_values(**data["penre"])
+                utility.lca_data.penrm.set_values(**data["penrm"])
+                utility.lca_data.penrt.set_values(**data["penrt"])
+                utility.lca_data.sm.set_values(**data["sm"])
+                utility.lca_data.rsf.set_values(**data["rsf"])
+                utility.lca_data.nrsf.set_values(**data["nrsf"])
+                utility.lca_data.fw.set_values(**data["fw"])
+                utility.lca_data.hwd.set_values(**data["hwd"])
+                utility.lca_data.nhwd.set_values(**data["nhwd"])
+                utility.lca_data.rwd.set_values(**data["rwd"])
+                utility.lca_data.cru.set_values(**data["cru"])
+                utility.lca_data.mfr.set_values(**data["mfr"])
+                utility.lca_data.mer.set_values(**data["mer"])
+                utility.lca_data.eee.set_values(**data["eee"])
+                utility.lca_data.eet.set_values(**data["eet"])
+                utility.lca_data.gwp.set_values(**data["gwp"])
+                utility.lca_data.odp.set_values(**data["odp"])
+                utility.lca_data.pocp.set_values(**data["pocp"])
+                utility.lca_data.ap.set_values(**data["ap"])
+                utility.lca_data.ep.set_values(**data["ep"])
+                utility.lca_data.adpe.set_values(**data["adpe"])
+                utility.lca_data.adpf.set_values(**data["adpf"])
+
+                return # lca data only need to be loaded once
 
