37a3764abf9f4177f380cfc1461ae4a1fdbc8c67
claraschm <142982090+claraschm@users.noreply.github.com>
2023-12-27 19:31:59 +0100

    calculation of LCA units

--- teco/logic/buildingobjects/buildingsystems/heat_supply_system.py
+++ teco/logic/buildingobjects/buildingsystems/heat_supply_system.py
@@ -3,6 +3,8 @@
 
 """This module includes a class for the Heat Supply System
 """
+import math
+
 from teaser.teaser.logic.buildingobjects.buildingsystems.heat_supply_system import HeatSupplySystem
 from teco.logic.buildingobjects.buildingphysics.en15804lcadata import En15804LcaData
 
@@ -117,38 +119,38 @@ class HeatSupplySystem(HeatSupplySystem):
         lca_data = En15804LcaData()
         lca_data.ref_flow_unit = "kg"
 
-        if self.parent.pipe_routing_heating == "centralised outside":
+        if self._pipe_routing_heating == "centralised outside":
 
-            length_horizontal_heating = 4 * self.parent.length_char - 10
-            length_strand_heating = self.parent.length_char * self.parent.parent.number_of_floors
-            length_tethers_heating = self.parent.length_char * self.parent.parent.number_of_floors * 2 / 3
+            length_horizontal_heating = 4 * self._length_char - 10
+            length_strand_heating = self._length_char * self.parent.number_of_floors
+            length_tethers_heating = self._length_char * self.parent.number_of_floors * 2 / 3
 
-        elif self.parent.pipe_routing_heating == "centralised inside":
+        elif self._pipe_routing_heating == "centralised inside":
 
-            length_horizontal_heating = 2 * self.parent.length_char - 10
-            length_strand_heating = self.parent.length_char * self.parent.parent.number_of_floors / 2
-            length_tethers_heating = self.parent.length_char * self.parent.parent.number_of_floors * 4
+            length_horizontal_heating = 2 * self._length_char - 10
+            length_strand_heating = self._length_char * self.parent.number_of_floors / 2
+            length_tethers_heating = self._length_char * self.parent.number_of_floors * 4
 
-        elif self.parent.pipe_routing_heating == "decentralised":
+        elif self._pipe_routing_heating == "decentralised":
 
-            length_strand_heating = self.parent.length_char * self.parent.parent.number_of_floors * 6
-            length_tethers_heating = self.parent.length_char * self.parent.parent.number_of_floors
+            length_strand_heating = self._length_char * self.parent.number_of_floors * 6
+            length_tethers_heating = self._length_char * self.parent.number_of_floors
 
-        if self.parent.pipe_routing_water == "centralised with circulation":
+        if self._pipe_routing_water == "centralised with circulation":
 
-            length_horizontal_water = 2 * self.parent.length_char - 10
-            length_strand_water = self.parent.length_char * self.parent.parent.number_of_floors / 2
-            length_stubs_water = self.parent.length_char * self.parent.parent.number_of_floors / 2
+            length_horizontal_water = 2 * self._length_char - 10
+            length_strand_water = self._length_char * self.parent.number_of_floors / 2
+            length_stubs_water = self._length_char * self.parent.number_of_floors / 2
 
-        elif self.parent.pipe_routing_water == "centralised without circulation":
+        elif self._pipe_routing_water == "centralised without circulation":
 
-            length_horizontal_water = self.parent.length_char - 5
-            length_strand_water = self.parent.length_char * self.parent.parent.number_of_floors / 4
-            length_stubs_water = self.parent.length_char * self.parent.parent.number_of_floors / 2
+            length_horizontal_water = self._length_char - 5
+            length_strand_water = self._length_char * self.parent.number_of_floors / 4
+            length_stubs_water = self._length_char * self.parent.number_of_floors / 2
 
-        elif self.parent.pipe_routing_water == "decentralised":
+        elif self._pipe_routing_water == "decentralised":
 
-            length_stubs_water = self.parent.length_char * self.parent.parent.number_of_floors / 2
+            length_stubs_water = self._length_char * self.parent.number_of_floors / 2
 
         length_pipes_heating = length_horizontal_heating + length_strand_heating + length_tethers_heating
         length_pipes_water = length_horizontal_water + length_strand_water + length_stubs_water
@@ -158,21 +160,141 @@ class HeatSupplySystem(HeatSupplySystem):
 
     # auch service life integrieren bei folgenden Formeln (aus ÖKOBAUDAT)
     def _lca_data_heat_generation(self):
-        # auch Öl- und Gastank
         lca_data = En15804LcaData()
         lca_data.ref_flow_unit = "pcs"
 
+        if self.heat_system == "gas":
+            if self._heat_generation == "circulating water heater":
+                lca_data = math.ceil(self.parent.simulated_heat_load / 1000 / 20) * lca_cwh  # 20 kW [pcs]
+            elif self._heat_generation == "low temperature":
+                sizes = [400, 120, 20]
+                count = {}
+                volume = self.parent.simulated_heat_load / 1000
+                for size in sorted(sizes, reverse=True):
+                    count = volume // size
+                    if count > 0:
+                        count[size] = count
+                        volume %= size
+                lca_data = count[400] * lca_400 + count[120] * lca_120 + count[20] * lca_20
+            else:
+                sizes = [400, 120, 20]
+                count = {}
+                volume = self.parent.simulated_heat_load / 1000
+                for size in sorted(sizes, reverse=True):
+                    count = volume // size
+                    if count > 0:
+                        count[size] = count
+                        volume %= size
+                lca_data = count[400] * lca_400 + count[120] * lca_120 + count[20] * lca_20
+
+            if self.parent.net_leased_area <= 180:
+                if self.parent.year_of_construction >= 2016 or self._year_of_retrofit >= 2016:
+                    lca_data = lca_data + lca_tank_2700
+                else:
+                    if self.parent.net_leased_area <= 130:
+                        lca_data = lca_data + lca_tank_2700
+                    else:
+                        lca_data = lca_data + lca_tank_4850
+
+            else:
+                if self.parent.net_leased_area >= 210:
+                   lca_data = lca_data + lca_tank_6400
+                else:
+                    lca_data = lca_data + lca_tank_4850
+
+        elif self._heat_system == "oil":
+            if self._heat_generation == "condensing":
+                sizes = [400, 120, 20]
+                count = {}
+                volume = self.parent.simulated_heat_load / 1000
+                for size in sorted(sizes, reverse=True):
+                    count = volume // size
+                    if count > 0:
+                        count[size] = count
+                        volume %= size
+                lca_data = count[400] * lca_400 + count[120] * lca_120 + count[20] * lca_20
+            else:
+                sizes = [400, 120, 20]
+                count = {}
+                volume = self.parent.simulated_heat_load / 1000
+                for size in sorted(sizes, reverse=True):
+                    count = volume // size
+                    if count > 0:
+                        count[size] = count
+                        volume %= size
+                lca_data = count[400] * lca_400 + count[120] * lca_120 + count[20] * lca_20
+
+            if self.parent.year_of_construction >= 2016 or self._year_of_retrofit >= 2016:
+                lca_data = math.ceil(self.parent.net_leased_area / 100) * lca_1500  # 1500 l [pcs]
+            else:
+                lca_data = math.ceil(self.parent.net_leased_area / 100) * lca_1500 * 2  # 3000 l [pcs]
+
+        elif self._heat_system == "electricity":
+            if self._heat_generation == "night storage":
+                    lca_data = math.ceil(self.parent.simulated_heat_load / 1000 / 21) + lca_data_ns  # 21 kW [pcs]
+            elif self._heat_generation == "heatpump air":
+                sizes = [14, 10, 7]
+                count = {}
+                volume = self.parent.simulated_heat_load / 1000
+                for size in sorted(sizes, reverse=True):
+                    count = volume // size
+                    if count > 0:
+                        count[size] = count
+                        volume %= size
+                lca_data = count[14] * lca_14 + count[10] * lca_10 + count[7] * lca_7
+            else:
+                sizes = [70, 20, 10]
+                count = {}
+                volume = self.parent.simulated_heat_load / 1000
+                for size in sorted(sizes, reverse=True):
+                    count = volume // size
+                    if count > 0:
+                        count[size] = count
+                        volume %= size
+                lca_data = count[70] * lca_70 + count[20] * lca_20 + count[10] * lca_10 + \
+                           count[70] * lca_70_pipes + count[20] * lca_20_pipes + count[10] * lca_10_pipes
+        elif self._heat_system == "biomass":
+            sizes = [120, 20]
+            count = {}
+            volume = self.parent.simulated_heat_load / 1000
+            for size in sorted(sizes, reverse=True):
+                count = volume // size
+                if count > 0:
+                    count[size] = count
+                    volume %= size
+            lca_data = count[120] * lca_120 + count[20] * lca_20
+        else:
+            lca_data = lca_distirct
+
+
     def _lca_data_buffer_storage(self):
         # water
+        lca_data = En15804LcaData()
+        lca_data.ref_flow_unit = "pcs"
 
     def _lca_data_pump(self):
         #Umwälzpumpe
+        lca_data = En15804LcaData()
+        lca_data.ref_flow_unit = "pcs"
 
     def _lca_data_solar(self):
         #Kollektor
+        lca_data = En15804LcaData()
+        lca_data.ref_flow_unit = "pcs"
+
 
     def _lca_data_radiator(self):
         # FB oder Heizkörper
+        lca_data = En15804LcaData()
+        lca_data.ref_flow_unit = "pcs"
+        if "heatpump" in self._heat_generation:
+            lca_data = self.parent.net_leased_area * lca_data_fbheizung  # floor heating 100mm distance [m^2]
+        else:
+            # todo andere Temperaturen?
+            if self._design_temp_flow == 55:
+                lca_data = self.parent.simulated_heat_load / 735 * 31.3 * lca_data_heizkörper  # radiator Type 22 0,5 m * 1 m [kg]
+            elif self._design_temp_flow == 70:
+                lca_data = self.parent.simulated_heat_load / 1169 * 31.3 * lca_data_heizkörper # radiator Type 22 0,5 m * 1 m [kg]
 
     def _lca_data_pe(self, pe_heating, pe_water):
         """Calculates the total annual primary energy demand of the heat_supply_system
